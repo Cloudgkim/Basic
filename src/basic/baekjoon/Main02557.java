627c48b5da5ec5deafa29b940ea2054ba7cb55a3
DESKTOP-KCG86FA\82109 <ehdrbs0217@naver.com>
2022-01-22 18:17:03 +0900

    Hello World We love kriii 고양이

---
+++ src/basic/baekjoon/Main02557.java
@@ -0,0 +1,14 @@
+package basic.baekjoon;
+
+
+public class Main02557 {
+
+    public static void main(String[] args) {
+    	// 1. 입출력과 사칙연산 1
+    	// Hello World!를 출력하시오.
+        System.out.println("Hello World!");
+         
+    }
+}
+
+
---
+++ src/basic/baekjoon/Main10171.java
@@ -0,0 +1,17 @@
+package basic.baekjoon;
+
+
+public class Main10171 {
+    
+    public static void main(String[] args){
+    	// 1. 입출력과 사칙연산 3
+    	// 고양이를 출력한다.
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+            
+    }
+}
+
+
