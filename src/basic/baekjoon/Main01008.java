26690b4ce068f6b353727df978fd3722a7323ed6
DESKTOP-KCG86FA\82109 <ehdrbs0217@naver.com>
2022-01-24 12:57:07 +0900

    A * B A / B

--- src/basic/baekjoon/Main01008.java
+++ src/basic/baekjoon/Main01008.java
@@ -6,14 +6,14 @@ public class Main01008 {
     
     public static void main(String[] args){
     	// 1. 입출력과 사칙연산 8
-    	// 첫째 줄에 A+B를 출력한다.
+    	// ﻿첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.﻿
     	
         Scanner sc = new Scanner(System.in);
         
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        Double a = sc.nextDouble();
+        Double b = sc.nextDouble();
         
-        System.out.print(a+b);
+        System.out.print(a/b);
         
     }
 }
--- src/basic/baekjoon/Main10998.java
+++ src/basic/baekjoon/Main10998.java
@@ -6,14 +6,14 @@ public class Main10998 {
     
     public static void main(String[] args){
     	// 1. 입출력과 사칙연산 7
-    	// 첫째 줄에 A+B를 출력한다.
+    	// 첫째 줄에 A*B를 출력한다.
     	
         Scanner sc = new Scanner(System.in);
         
         int a = sc.nextInt();
         int b = sc.nextInt();
         
-        System.out.print(a+b);
+        System.out.print(a*b);
         
     }
 }
