da44b7b527db6b028a542d507e01beccfd3ac920
DESKTOP-KCG86FA\82109 <ehdrbs0217@naver.com>
2022-01-26 21:01:02 +0900

    ??! 1998년생인 내가 태국에서는 2541년생?!

---
+++ src/basic/baekjoon/Main10926.java
@@ -0,0 +1,18 @@
+package basic.baekjoon;
+
+import java.util.Scanner;
+
+public class Main10926 {
+    
+    public static void main(String[] args){
+    	// 1. 입출력과 사칙연산 10
+    	// 첫째 줄에 준하의 놀람을 출력한다. 놀람은 아이디 뒤에 ??!를 붙여서 나타낸다.
+    	
+		Scanner sc = new Scanner(System.in);
+		String id = sc.next();
+		System.out.println(id + "??!");
+        
+    }
+}
+
+
---
+++ src/basic/baekjoon/Main18108.java
@@ -0,0 +1,18 @@
+package basic.baekjoon;
+
+import java.util.Scanner;
+
+public class Main18108 {
+    
+    public static void main(String[] args){
+    	// 1. 입출력과 사칙연산 11
+    	// 불기 연도를 서기 연도로 변환한 결과를 출력한다.
+    	
+		Scanner sc = new Scanner(System.in);
+		int y = sc.nextInt();
+        System.out.print(y - 543);
+        
+    }
+}
+
+
